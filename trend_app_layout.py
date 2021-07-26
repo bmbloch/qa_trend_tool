558a0c353b6facc59af8ed9f6e676252bd9799d1
Ben <ben.bloch@moodys.com>
2021-07-26 10:58:55 -0400

    Putting buttons in navbar, will now display on all tabs

--- trend_app_layout.py
+++ trend_app_layout.py
@@ -6,6 +6,49 @@ import dash_table
 import dash_daq as daq
 
 def get_app_layout():
+
+    navbar = dbc.Navbar(
+    [
+        html.Div([  
+            dbc.Row(
+            dbc.Col(
+                dbc.Button('Export Data',id='download-button',color='primary',block=True, size='sm'),
+                width=20
+                    ),
+                justify='center'
+                    ),
+                ], style={'display': 'inline-block', 'padding-left': '1450px'}),  
+        html.Div([
+            dbc.Row(
+            dbc.Col(
+                dbc.Button('Export Flags',id='flag-button',color='warning',block=True, size='sm'),
+                width=20
+                    ),
+                justify='center'
+                    ),
+                ], style={'display': 'inline-block', 'padding-left': '50px'}),
+        html.Div([
+            dbc.Row(
+            dbc.Col(
+                dbc.Button('Finalize Trend',id='finalize-button',color='success',block=True, size='sm'),
+                width=20
+                    ),
+                justify='center'
+                    ),
+                ], style={'display': 'inline-block', 'padding-left': '50px'}),
+        html.Div([
+            dbc.Row(
+            dbc.Col(
+                dbc.Button('Logout',id='logout-button',color='danger',block=True, size='sm'),
+                width=20
+                    ),
+                justify='center'
+                    ),
+                ], style={'display': 'inline-block', 'padding-left': '50px'}),
+    ],
+    fixed='top'
+)
+
     return \
         html.Div([
             dcc.Location(id='home-url',pathname='/home'),
@@ -132,44 +175,6 @@ def get_app_layout():
                                 ], style={'display': 'none'}, id='flag_filt_container'),
                         ], style={'width': '35%', 'display': 'inline-block', 'padding-left': '30px', 'padding-top': '78px'}),
                     html.Div([
-                        html.Div([
-                            html.Div([  
-                                dbc.Row(
-                                dbc.Col(
-                                    dbc.Button('Export Data',id='download-button',color='primary',block=True,size='sm'),
-                                    width=20
-                                        ),
-                                    justify='center'
-                                        ),
-                                    ], style={'display': 'inline-block', 'padding': '20px'}),  
-                            html.Div([
-                                dbc.Row(
-                                dbc.Col(
-                                    dbc.Button('Export Flags',id='flag-button',color='warning',block=True,size='sm'),
-                                    width=20
-                                        ),
-                                    justify='center'
-                                        ),
-                                    ], style={'display': 'inline-block', 'padding': '20px'}),
-                            html.Div([
-                                dbc.Row(
-                                dbc.Col(
-                                    dbc.Button('Finalize Trend',id='finalize-button',color='success',block=True,size='sm'),
-                                    width=20
-                                        ),
-                                    justify='center'
-                                        ),
-                                    ], style={'display': 'inline-block', 'padding': '20px'}),
-                            html.Div([
-                                dbc.Row(
-                                dbc.Col(
-                                    dbc.Button('Logout',id='logout-button',color='danger',block=True,size='sm'),
-                                    width=20
-                                        ),
-                                    justify='center'
-                                        ),
-                                    ], style={'display': 'inline-block', 'padding': '20px'}),
-                            ], style={'display': 'block', 'padding-left': '325px'}),
                         html.Div([
                             html.Div([
                                 html.Div([
@@ -677,6 +682,7 @@ def get_app_layout():
                             ], style={'display': 'none'}, id='met_rank_container'),
                         ], style={'display': 'block'}),
                     ]),
-                ]),
-            ])
+                ], style={'padding-top': '50px'}),
+            ]),
+            html.Div([navbar]),
         ])
