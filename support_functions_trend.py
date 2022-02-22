42d0c6e5badd69d9daf55c81199ced6fc867b9e5
Ben Bloch <ben.bloch@moodys.com>
2022-02-22 16:58:04 -0500

    better handling identification of shim year and shim month

--- support_functions_trend.py
+++ support_functions_trend.py
@@ -696,12 +696,12 @@ def manual_rebench_check(data, data_temp, rebench_to_check, curryr, currmon, sec
     if len(rebench_to_check) > 0:
         check = True
         first_yr = rebench_to_check.reset_index().loc[0]['identity_row']
-        if currmon < 10:
-            first_month = first_yr[-1]
-            first_yr = first_yr[-5:-1]
-        else:
+        if len(first_yr.split(drop_val)[-1]) == 6:
             first_month = first_yr[-2:]
             first_yr = first_yr[-6:-2]
+        else:
+            first_month = first_yr[-1]
+            first_yr = first_yr[-5:-1]
     else:
         check = False
         first_yr = False
